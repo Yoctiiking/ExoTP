3378635e3c4667c4fbe52091493a069f1be3b48f
Yoctiiking <yedeyoctan@gmail.com>
2023-12-18 16:38:11 -0500

    Exo1_on_the_props

--- App.tsx
+++ App.tsx
@@ -1,118 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { View, Text, StyleSheet, ScrollViewComponent, ScrollView } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App(){
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const Card = ({title, content, children}) => {
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    return(
+          <View style = {style.constainer}>
+            <Text style = {style.title}>{title}</Text>
+            <Text style = {style.content}>{content}</Text>
+            
+            { children ?(
+              <View style = {style.supView}>
+                  <Text style = {style.supContent}>{children}</Text>
+              </View>
+            ) : null}
+
+          </View>
+    )
+  }
+  return(
+    <View>
+      <Card title={"AZERTY"} content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
+      <Card title={"lorem"} content={"lorem ipsum"}></Card>
     </View>
-  );
+  )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const style = StyleSheet.create({
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  constainer:{
+    width: "90%",
+    height: "auto",
+    padding: 10,
+    borderRadius: 15,
+    backgroundColor: "green",
+    alignSelf: "center",
+    marginTop: "5%",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  title:{
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "orange",
+    left: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+
+  content:{
     fontSize: 18,
-    fontWeight: '400',
+    color: "white",
+    left: 10,
+    textAlign: "justify",
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+  supView:{
+      left: "60%",
+      width: "40%",
+      height: "auto",
+      padding: 5,
+      marginTop: 10,
+      marginBottom: 0,
+      borderRadius: 10,
+      backgroundColor: "orange",
+  },
+
+  supContent:{
+    fontSize: 15,
+    textAlign: "center",
+    color: "#ff0066",
+  }
+})
