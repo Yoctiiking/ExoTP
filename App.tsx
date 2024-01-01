c4344f90348338ea8367a9806b60d886ab8490e6
Yoctiiking <yedeyoctan@gmail.com>
2024-01-01 03:23:46 -0500

    Exo3_on_the_TextInput_4

--- App.tsx
+++ App.tsx
@@ -1,34 +1,69 @@
-//Exo3_on_the_TextInput_3
+//Exo3_on_the_TextInput_4
 import React, { useState } from 'react';
 import {
   View,
   Text,
+  Button,
   SafeAreaView,
   StyleSheet,
   TextInput,
   ScrollView,
   Alert,
+  ToastAndroid,
 } from 'react-native';
 
 export default function App() {
-  const [text1, setText1] = useState('');
-  const [text2, setText2] = useState('');
+
+  const [identifiants, setIndentifiants] = useState([
+      {
+        placeholder: 'Nom d\'utilisateur',
+        value:"",
+        },
+      {
+        placeholder: 'Mot de passe',
+        value: "",
+      }
+    ]);
+
+  const updateIdentifiants = (value, index) => {
+    const stageId = [...identifiants];
+    stageId[index].value = value;
+    setIndentifiants(stageId);
+  }
+
+  const handlePress = () => {
+    console.log(identifiants.map(item => item.value));
+    if(identifiants[0].value !== ''){
+      Alert.alert('Login ID',
+      `Username: ${identifiants[0].value}\nPassword: ${identifiants[1].value}`,
+      [{
+        text: 'Ok',
+        style: 'default',
+      }]);
+    } else {
+      ToastAndroid.show("Enter your username", 3);
+    }
+  }
 
   return (
-    <View style = {styles.container}>
-      <TextInput style={styles.textInput}
-        onChangeText={(text) => setText1(text)}
-        value={text1}
-        placeholder="Text 1"
-      />
-      <Text style={{fontSize: 20}}>You typed: {text1}</Text>
-
-      <TextInput style={styles.textInput}
-        onChangeText={(text) => setText2(text)}
-        value={text2}
-        placeholder="Text 2"
-      />
-      <Text style={{fontSize: 20}}>You typed: {text2}</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style = {{fontSize: 50, textAlign: 'center', fontWeight: 'bold'}}>LOGIN</Text>
+        {identifiants.map( (value, index) => (
+          <TextInput style={styles.textInput}
+            key = {index}
+            value = {value.value}
+            placeholder = {value.placeholder}
+            onChangeText = {(val) => updateIdentifiants(val, index)}
+           />
+        ))}
+        <View style={styles.buttonView}>
+          <Button 
+            title = "Connect"
+            onPress = {handlePress}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -37,13 +72,19 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'plum',
     flex: 1,
+    paddingTop: "10%",
   },
   textInput: {
     textAlign: 'center',
     borderColor: 'black',
     borderWidth: 2,
     alignSelf: 'center',
-    marginTop: '10%',
+    marginTop: '5%',
     width: '80%',
+  },
+
+  buttonView: {
+    marginTop: 20,
+    alignSelf: 'center',
   },
 });
