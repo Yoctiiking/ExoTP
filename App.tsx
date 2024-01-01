0a3e27d39c452df83fba2612fe4e263c89fb4912
Yoctiiking <yedeyoctan@gmail.com>
2024-01-01 00:52:40 -0500

    Exo3_on_the_TextInput_3

--- App.tsx
+++ App.tsx
@@ -1,23 +1,49 @@
-//Exo3_on_the_TextInput_2
+//Exo3_on_the_TextInput_3
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Alert,
+} from 'react-native';
 
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+export default function App() {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
 
-const ChildComponent = ({name}) => {
   return (
-    <View>
-      <Text>{name}</Text>
+    <View style = {styles.container}>
+      <TextInput style={styles.textInput}
+        onChangeText={(text) => setText1(text)}
+        value={text1}
+        placeholder="Text 1"
+      />
+      <Text style={{fontSize: 20}}>You typed: {text1}</Text>
+
+      <TextInput style={styles.textInput}
+        onChangeText={(text) => setText2(text)}
+        value={text2}
+        placeholder="Text 2"
+      />
+      <Text style={{fontSize: 20}}>You typed: {text2}</Text>
     </View>
   );
-};
-
-export default function ParentComponent() {
-
-  const [info, setInfo] = useState("Voici l'info")
-
-  return (
-    <View>
-      <ChildComponent name = {info} />
-    </View>
-  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'plum',
+    flex: 1,
+  },
+  textInput: {
+    textAlign: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+    alignSelf: 'center',
+    marginTop: '10%',
+    width: '80%',
+  },
+});
